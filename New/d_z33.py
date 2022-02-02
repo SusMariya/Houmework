afc74dc8be4c2757aa692f83674631324b31081b
Marya_S <suspitcina.marya@gmail.com>
2022-02-02 14:05:57 +0700

    d_z33

--- New/d_z33.py
+++ New/d_z33.py
@@ -1,23 +1,25 @@
+
+### Чтобы вы подумали что не пыталась сделать домашнее задание, я пыталась, мой вариант написан первым, но
+# понимаю что мой не правильный(((
+
 class Person:
     def __init__(self, surname, name, age):
         self.surname = surname
         self.name = name
         self.age = age
-
-    @classmethod
-    def sort(cls, surname):
-        pass
-
+    def __repr__(self):
+        return f"{self.surname} {self.name} {self.age}"
 
 class SortKey:
-    def __init__(self, surname):
-    # def __init__(self, surname, name, age):
-    #     super().__init__(surname, name, age)
-        self.__surname = surname
+    def __init__(self, personlst):
+        self.__personlst = personlst
+
 
     def __call__(self, *args, **kwargs):
-        return Person.sort(self.__surname)
-
+        if len(args) == 1 and args[0] == 'surname':
+            return sorted(self.__personlst, key=lambda Person: Person.surname)
+        elif len(args) == 2 and args[0] == 'surname' and args[1] == "name":
+            return sorted(self.__personlst, key=lambda Person:  Person.name)
 
 
 p = [Person('Иванов', 'Игорь', 28),
@@ -26,5 +28,47 @@ p = [Person('Иванов', 'Игорь', 28),
      Person('Петров', 'Анатолий', 11),
      Person('Иванов', 'Иван', 28)]
 g = SortKey(p)
-for i in list(p):
-    print(g())
+print(g("surname"))
+
+##########################################
+
+
+class Person:
+    def __init__(self, surname, name, age):
+        self.surname = surname
+        self.name = name
+        self.age = age
+
+    @property
+    def data(self):
+        return self.surname, self.name, self.age
+
+
+class SortKey:
+    def __init__(self, *args):
+        self.__personlst = args
+
+    def __call__(self, lst):
+        lst.sort(key=lambda j: [j.__dict__[key] for key in self.__personlst])
+
+
+p = [Person('Иванов', 'Игорь', 28),
+     Person('Петров', 'Степан', 21),
+     Person('Сидоров', 'Антон', 25),
+     Person('Петров', 'Анатолий', 11),
+     Person('Иванов', 'Иван', 28)]
+
+for i in p:
+    print(i.data)
+print()
+
+s1 = SortKey("surname")
+s1(p)
+for i in p:
+    print(i.data)
+print()
+s2 = SortKey("surname", "name")
+s2(p)
+for i in p:
+    print(i.data)
+print()
